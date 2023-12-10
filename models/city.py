48c68b4bbd0ec23b3600353517cbbf506d9f4e92
zyadhany <zeyad.hany2003@gmail.com>
2023-12-10 19:47:11 +0200

    edietd task 9 by adding several class

--- models/city.py
+++ models/city.py
@@ -12,5 +12,14 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
+
+    """
+    This module contains the city class
+
+    Attributes:
+        state_id:string - state id
+        name:string - name of city
+    """
+
     state_id: str = ''
     name: str = ''
