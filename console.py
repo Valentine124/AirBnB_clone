4ed5c0f60b2434a013c15d044322f8892849f590
zyadhany <zeyad.hany2003@gmail.com>
2023-12-10 14:34:21 +0200

    made ttask 7

--- console.py
+++ console.py
@@ -7,8 +7,9 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
+from datetime import datetime
 
-MyClasses = {'BaseModel': BaseModel}
+MyClasses = ['BaseModel']
 
 
 def sprate(arg=""):
@@ -66,6 +67,90 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 print(objects[s])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = sprate(arg)
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in MyClasses:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            s = "{}.{}".format(args[0], args[1])
+            if s not in objects:
+                print("** no instance found **")
+            else:
+                del objects[s]
+                storage.save()
+
+    def do_all(self, arg):
+        args = sprate(arg)
+        opj = MyClasses
+        res = []
+
+        if len(args):
+            if args[0] not in MyClasses:
+                print("** class doesn't exist **")
+                return 0
+            opj = [args[0]]
+
+        for m in storage.all().values():
+            if m.__class__.__name__ in opj:
+                res.append(m.__str__())
+        print(res)
+
+    def do_all(self, arg):
+        args = sprate(arg)
+        opj = MyClasses
+        res = []
+
+        if len(args):
+            if args[0] not in MyClasses:
+                print("** class doesn't exist **")
+                return 0
+            opj = [args[0]]
+
+        for m in storage.all().values():
+            if m.__class__.__name__ in opj:
+                res.append(m.__str__())
+        print(res)
+
+    def do_update(self, arg):
+        """ Updates an instance based on the class name and id."""
+        args = sprate(arg)
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return 0
+        elif args[0] not in MyClasses:
+            print("** class doesn't exist **")
+            return 0
+        elif len(args) == 1:
+            print("** instance id missing **")
+            return 0
+
+        objects = storage.all()
+        s = "{}.{}".format(args[0], args[1])
+        if s not in objects:
+            print("** no instance found **")
+            return
+
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return 0
+        elif len(args) == 3:
+            print("** value missing **")
+            return 0
+
+        X = objects[s]
+
+        X.__dict__[args[2]] = args[3]
+        X.updated_at = datetime.utcnow()
+        storage.save()
 
 
 if __name__ == '__main__':
