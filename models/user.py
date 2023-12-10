15ece2f15bf77da21d16e24da54242ad95cd4d57
zyadhany <zeyad.hany2003@gmail.com>
2023-12-10 18:54:49 +0200

    made task 8 by adding User class

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This module contains the user class
+"""
+
+
+import uuid
+import datetime as dt
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    email: str = ''
+    password: str = ''
+    first_name: str = ''
+    last_name: str = ''
