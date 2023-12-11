b288ba441c5762dbd384e870a255e7f51957ccaa
zyadhany <zeyad.hany2003@gmail.com>
2023-12-11 02:50:43 +0200

    test BaseModel #10d

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,7 @@ import json
 import os.path
 from models.base_model import BaseModel
 from models.city import City
+from models.place import Place
 from models.state import State
 from models.amenity import Amenity
 from models.review import Review
