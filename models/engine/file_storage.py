f0d35993026c568d20ad7d75c440dd80517820b4
zyadhany <zeyad.hany2003@gmail.com>
2023-12-11 02:49:49 +0200

    test BaseModel #10c

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,7 @@ from models.base_model import BaseModel
 from models.city import City
 from models.state import State
 from models.amenity import Amenity
+from models.review import Review
 from models.user import User
 
 
--- models/place.py
+++ models/place.py
@@ -22,4 +22,4 @@ class Place(BaseModel):
     price_by_night: int = 0
     latitude: float = 0.0
     longitude:  float = 0.0
-    amenity_ids:  list[str] = []
+    amenity_ids:  []
