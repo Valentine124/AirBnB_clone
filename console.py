e36ea352ab592579e07d0b522cdde54bbf46d86d
zyadhany <zeyad.hany2003@gmail.com>
2023-12-11 02:53:29 +0200

    test BaseModel #10d

--- console.py
+++ console.py
@@ -7,10 +7,16 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
 from models.user import User
 from datetime import datetime
 
-MyClasses = ['BaseModel', 'User']
+MyClasses = ['BaseModel', 'User', 'Place', 'State',
+             'City', 'Amenity', 'Review']
 
 
 def sprate(arg=""):
